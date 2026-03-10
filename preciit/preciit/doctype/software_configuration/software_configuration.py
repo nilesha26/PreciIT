18b3cbc2478c9f1f658383a7b39daaae639b9041
nilesha26 <nilesha@preciholesports.com>
2026-03-10 14:20:36 +0530

    If the software configuration doc submitt then the asset status change

--- preciit/preciit/doctype/software_configuration/software_configuration.py
+++ preciit/preciit/doctype/software_configuration/software_configuration.py
@@ -7,12 +7,18 @@ from frappe.model.naming import make_autoname
 
 
 class SoftwareConfiguration(Document):
+
 	def autoname(self):
 		prefix = self.item.replace(" ", "").upper()
 		self.name = make_autoname(f"{prefix}-.##")
-	
-	def on_update(self):
 
+	def after_insert(self):
+		self.update_asset_status()
+
+	def on_update(self):
+		self.update_asset_status()
+
+	def update_asset_status(self):
 		if not self.item:
 			return
 
